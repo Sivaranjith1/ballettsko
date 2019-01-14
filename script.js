0b9d11db83ef85dcaad5490ea0d14df93466d61a
fataklut <44358596+fataklut@users.noreply.github.com>
2019-01-14 09:56:11 +0100

    new

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const valuta = [{
-        navn: 'Euro',
-        kurs: 8,
-        img: "img/valuta/euro.jpg"
-    },
-    {
-        navn: 'NOK',
-        kurs: 1,
-        img: "img/valuta/nok.jpg"
-    },
-    {
-        navn: 'USD',
-        kurs: 6,
-        img: "img/valuta/usa.jpg"
-    },
-    {
-        navn: 'GBP',
-        kurs: 9,
-        img: "img/valuta/gb.jpg"
-    },
-    {
-        navn: 'AUD',
-        kurs: 7,
-        img: "img/valuta/aud.jpg"
-    },
+    navn: 'Euro',
+    kurs: 8,
+    img: "img/valuta/euro.jpg"
+},
+{
+    navn: 'NOK',
+    kurs: 1,
+    img: "img/valuta/nok.jpg"
+},
+{
+    navn: 'USD',
+    kurs: 6,
+    img: "img/valuta/usa.jpg"
+},
+{
+    navn: 'GBP',
+    kurs: 9,
+    img: "img/valuta/gb.jpg"
+},
+{
+    navn: 'AUD',
+    kurs: 7,
+    img: "img/valuta/aud.jpg"
+},
 ]
 
 
@@ -35,5 +35,9 @@ function konverterPris(nok, index) {
 }
 
 $(function () {
+    $(document).on("click", function () {
+        $("#sidenav").animate({ width: 'toggle' }, 350);
+        $('#content').toggleClass('full shade');
 
+    });
 })
