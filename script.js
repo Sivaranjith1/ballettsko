a7351937a2ab225d8ff532980f1a80771cba90f4
fataklut <44358596+fataklut@users.noreply.github.com>
2019-01-14 09:02:57 +0100

    new

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const valuta = [{
-        navn: 'Euro',
-        kurs: 8,
-        img: "euro.jpg"
-    },
-    {
-        navn: 'NOK',
-        kurs: 1,
-        img: "nok.jpg"
-    },
-    {
-        navn: 'USD',
-        kurs: 6,
-        img: ""
-    },
-    {
-        navn: 'GBP',
-        kurs: 9,
-        img: "gb.jpg"
-    },
-    {
-        navn: 'AUD',
-        kurs: 7,
-        img: "aud.jpg"
-    },
-]
+    navn: 'Euro',
+    kurs: 8,
+    img: "euro.jpg"
+},
+{
+    navn: 'NOK',
+    kurs: 1,
+    img: "nok.jpg"
+},
+{
+    navn: 'USD',
+    kurs: 6,
+    img: ""
+},
+{
+    navn: 'GBP',
+    kurs: 9,
+    img: "gb.jpg"
+},
+{
+    navn: 'AUD',
+    kurs: 7,
+    img: "aud.jpg"
+},
+];
+
+$(function () {
+
+})
