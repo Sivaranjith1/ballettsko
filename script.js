e0b7cab01ffcced46c910152959a8ed643cfc5a2
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2019-01-14 08:46:38 +0100

    valuta

---
+++ script.js
@@ -0,0 +1,26 @@
+const valuta = [{
+        navn: 'Euro',
+        kurs: 8,
+        img: "euro.jpg"
+    },
+    {
+        navn: 'NOK',
+        kurs: 1,
+        img: "nok.jpg"
+    },
+    {
+        navn: 'USD',
+        kurs: 6,
+        img: ""
+    },
+    {
+        navn: 'GBP',
+        kurs: 9,
+        img: "gb.jpg"
+    },
+    {
+        navn: 'AUD',
+        kurs: 7,
+        img: "aud.jpg"
+    },
+]
