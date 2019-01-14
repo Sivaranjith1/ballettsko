973e0e9ad2c077345c145f86c20c9528367ed702
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2019-01-14 08:56:58 +0100

    gradient

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 const valuta = [{
         navn: 'Euro',
         kurs: 8,
-        img: "euro.jpg"
+        img: "img/valuta/euro.jpg"
     },
     {
         navn: 'NOK',
         kurs: 1,
-        img: "nok.jpg"
+        img: "img/valuta/nok.jpg"
     },
     {
         navn: 'USD',
         kurs: 6,
-        img: ""
+        img: "img/valuta/usa.jpg"
     },
     {
         navn: 'GBP',
         kurs: 9,
-        img: "gb.jpg"
+        img: "img/valuta/gb.jpg"
     },
     {
         navn: 'AUD',
         kurs: 7,
-        img: "aud.jpg"
+        img: "img/valuta/aud.jpg"
     },
 ]
+
+//konvertere valuta
+function konverterPris(nok, index) {
+    //index i array valuta
+    const kurs = valuta[index].kurs
+    const pris = nok / kurs
+    return pris
+}
