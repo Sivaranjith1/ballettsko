bd8b6d3984da6f38d15416edc72891be3c04d5d8
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2019-01-14 10:18:04 +0100

    flere sko

--- sko.js
+++ sko.js
@@ -1,6 +1,19 @@
 const sko = [{
-    img: 'img/sko/piruettUltra.jpg',
-    pris: '1000',
-    navn: 'Piruett Ultra',
-    objectFit: 'contain',
-}]
+        img: 'img/sko/piruettUltra.jpg',
+        pris: '1000',
+        navn: 'Piruett Ultra',
+        objectFit: 'contain',
+    },
+    {
+        img: 'img/sko/classicGalosh.jpg',
+        pris: '700',
+        navn: 'Classic Galosh Black',
+        objectFit: 'contain',
+    },
+    {
+        img: 'img/sko/classicGaloshOrange.jpg',
+        pris: '650',
+        navn: 'Classic Galosh Orange',
+        objectFit: 'contain',
+    },
+]
